acef577d0b1892f3f4faaa36422f8e8a2ff53e6e
nguyenthanh311003 <nguyenthanh311003@gmail.com>
2023-10-03 20:24:39 +0700

    add junit jar files and update main()

--- src/com/nguyenthanh/mathutil/main/Main.java
+++ src/com/nguyenthanh/mathutil/main/Main.java
@@ -16,6 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("More over, this message comes from a US-BUILT'S JAR FILE");
         tryTDDFirst();
         testFactorialGivenWrongArgumentThrowsException();
     }
